d9633454bc01c0f3125db512340169a8b7cdfda2
hectormtz22 <hec.gre81@gmail.com>
2020-11-27 00:41:28 -0600

    Embed Excel, PP and PDF

--- src/components/ResourceComponent.vue
+++ src/components/ResourceComponent.vue
@@ -1,7 +1,10 @@
 <template>
 	<main class="opened-resources">
 		<section v-for="(r, index) in resources" v-bind:key="index">
-			<iframe :src='"https://view.officeapps.live.com/op/embed.aspx?src=" + path + "/documents/PIA_ATI.docx"' width='100%' height='565px' frameborder='0'> </iframe>
+			<iframe :src='"https://view.officeapps.live.com/op/embed.aspx?src=" + path + r' width='100%' height='565px' frameborder='0'> </iframe>
+		</section>
+		<section v-for="(p, index) in pdf" v-bind:key="index">
+			<iframe :src='p' width='100%' height='565px' frameborder='0'> </iframe>
 		</section>
 	</main>
 </template>
@@ -10,7 +13,8 @@
 export default {
 	name: "ResourceComponent",
 	props: {
-		resources: Array
+		resources: Array,
+		pdf: Array
 	},
 	computed: {
 		path: () => {
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -2,7 +2,7 @@
 	<div>
     <Banner v-bind:text="text" img="banner-5"/>
 		<Content :img="img" :paragraph="paragraph" />
-		<ResourceComponent :resources="resources" />
+		<ResourceComponent :pdf="pdf" :resources="resources" />
 	</div>
 </template>
 
@@ -10,14 +10,15 @@
 import Banner from '@/components/Banner'
 import Content from '@/components/Content'
 import ResourceComponent from '@/components/ResourceComponent'
-import { resources } from '@/data/resources.json'
+import { resources, pdf } from '@/data/resources.json'
 
 export default {
 	name: "Resources",
 	data() {
 		return {			
 			text: "Recursos",
-			resources
+			resources,
+			pdf
 		}
 	},
 	components: {
