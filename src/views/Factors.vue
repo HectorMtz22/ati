b72082f362096624ac531a4e30da595cf0c9bc7f
hectormtz22 <hec.gre81@gmail.com>
2020-11-29 21:41:35 -0600

    Change PDF to Factores

--- src/views/Factors.vue
+++ src/views/Factors.vue
@@ -2,13 +2,19 @@
 	<div>
     <Banner v-bind:text="text" img="banner-3"/>
 		<Content :videoId="videoId" :img="img" :paragraph="paragraph" />
+		<main class="opened-resources">
+			<h4>Efectos del Desempleo</h4>
+			<section v-for="(p, index) in pdf" v-bind:key="index">
+				<iframe :src='p.path' width='100%' height='565px' frameborder='0'> </iframe>
+			</section>
+		</main>
 	</div>
 </template>
 
 <script>
 import Banner from '@/components/Banner';
 import Content from '@/components/Content';
-import { paragraph, img, videoId } from '@/data/factors.json';
+import { paragraph, img, videoId, pdf } from '@/data/factors.json';
 export default {
 	name: "Factors",
 	data() {
@@ -16,7 +22,8 @@ export default {
 			paragraph,
 			img,
 			text: "Factores",
-			videoId
+			videoId,
+			pdf
 		}
 	},
 	components: {
